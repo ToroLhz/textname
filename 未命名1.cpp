69dd22c16b4b038024beb0f26811092cfdb41e5b
ToroLhz <92197511+ToroLhz@users.noreply.github.com>
2021-11-19 19:44:43 +0800

    上传测试
    第一次测试上传

---
+++ 未命名1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main() {
+    std::ios::sync_with_stdio(0);
+    string a;
+    wstring b;
+    a="算法工作室牛逼";
+    b=L"算法工作室牛逼";
+    cout<<a<<endl;
+    wcout<<b<<endl;
+    cout<<a.length()<<endl;
+    cout<<b.length()<<endl;
+    return 0;
+}
